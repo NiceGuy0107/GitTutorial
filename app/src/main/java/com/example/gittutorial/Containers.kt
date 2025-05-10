67771df944fc67cc8cbb83c3f0b4ccf1a10f47bc
NiceGuy0107 <habushev2003@gmail.com>
2025-05-10 17:29:25 +0300

    Realization of the Containers function

--- app/src/main/java/com/example/gittutorial/Containers.kt
+++ app/src/main/java/com/example/gittutorial/Containers.kt
@@ -1,2 +1,19 @@
 package com.example.gittutorial
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+
+@Composable
+fun Containers() {
+    Column {
+        Box(modifier = Modifier.background(Color.Red).fillMaxWidth().weight(1f))
+        Box(modifier = Modifier.background(Color.DarkGray).fillMaxWidth().weight(1f))
+        Box(modifier = Modifier.background(Color.Blue).fillMaxWidth().weight(1f))
+    }
+}
